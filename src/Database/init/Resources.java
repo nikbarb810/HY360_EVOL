356c5b4c7d6226caeb931e970c4a7a48ef192338
nikbarb810 <93132694+nikbarb810@users.noreply.github.com>
2024-01-11 13:50:36 +0200

    Create Resources.java

---
+++ src/Database/init/Resources.java
@@ -0,0 +1,22 @@
+package Database.init;
+
+public class Resources {
+
+    // Customers
+
+    // Cars
+
+    // Motorcycles
+
+    // Scooters
+
+    // Bicycles
+
+    // Orders
+
+    // Bookings
+
+    // Expenses
+
+    // Payments
+}
